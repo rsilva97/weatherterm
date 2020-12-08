c60521f1dd74b8fbf9324624d1ba9597f8a1ef83
rsilva97 <rsilva@fandm.edu>
2020-12-08 00:01:02 -0500

    Adding updated files

--- weatherterm/core/unit.py
+++ weatherterm/core/unit.py
@@ -4,7 +4,7 @@ from .base_enum import BaseEnum
 @unique
 class Unit(Enum):
 	'''
-	Every property is set to auto(), meaning the value for every item in the enumeration will be set automatically for us.
+	Every property is set to auto(), meaning the value for every item in the enumeration will be set automatically for us. Since the Unit class inherits from BaseEnum, every time the auto() is called, the _generate_next_value_ method on BaseEnum will be invoked and will return the name of the property itself.
 	'''
 
 	CELSIUS = auto()
